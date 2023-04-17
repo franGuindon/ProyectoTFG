946b0d5726068668a50cb3f43a0be9eef9df6bdb
franGuindon <fbadilla10@gmail.com>
2023-04-16 20:07:25 -0600

    Update example to use videofilesource

--- artifact_detector/examples/feature_extractor_tool.cpp
+++ artifact_detector/examples/feature_extractor_tool.cpp
@@ -4,14 +4,20 @@
 
 #include "include/feature_extractor.hpp"
 #include "include/types.hpp"
+#include "include/videofilesource.hpp"
 
 int main(int argc, char **argv) {
+  ReturnValue ret;
+
+  auto file_src = std::make_unique<VideofileSource>("file.mp4");
   auto extractor = std::make_unique<FeatureExtractor>();
 
   Buffer<uint8_t> frame = {nullptr, 0, 0, 0};
   Buffer<float> features = {nullptr, 0, 0, 0};
 
-  ReturnValue error = extractor->extractFeatures(&frame, &features);
+  ret = file_src->pullFrame(&frame);
+  ret = extractor->extractFeatures(&frame, &features);
+  ret = file_src->pushFrame(&frame);
 
   return 0;
 }
