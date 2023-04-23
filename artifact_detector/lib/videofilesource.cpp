d27160f8c1f65c552f444e607dc406d3e9ad90fa
franGuindon <fbadilla10@gmail.com>
2023-04-23 13:00:01 -0600

    Update library build and add code to videofilesrc

--- artifact_detector/lib/videofilesource.cpp
+++ artifact_detector/lib/videofilesource.cpp
@@ -2,7 +2,17 @@
 
 #include "include/videofilesource.hpp"
 
-VideofileSource::VideofileSource(std::string filename) {}
+VideofileSource::VideofileSource(std::string filename) : file_{filename} {
+  ReturnValue ret;
+  ret = openFile();
+  if (ReturnCode::Success != ret.code) {
+    throw ret;
+  }
+}
+
+ReturnValue VideofileSource::openFile() {
+  return ReturnValue{ReturnCode::FileError, "File error"};
+}
 
 ReturnValue VideofileSource::pullFrame(Buffer<uint8_t>* frame) {
   return ReturnValue{ReturnCode::Success, "Operation returned successfully"};
