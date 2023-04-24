a228843eece8266ae56e05c0ca643ecbc7de9e7b
franGuindon <fbadilla10@gmail.com>
2023-04-23 20:07:30 -0600

    Update feature generation bash script for tests

--- tools/bash_tools/generate_feature_files.sh
+++ tools/bash_tools/generate_feature_files.sh
@@ -1,17 +1,18 @@
-if [[ $# -ne 2 ]]; then
-  echo "Usage generate_feature_files.sh [FEATURE_GENERATOR_BIN] [DATASET_DIR]";
+if [[ $# -ne 3 ]]; then
+  echo "Usage generate_feature_files.sh [FEATURE_GENERATOR_BIN] [DATASET_DIR] [OUTFILE_PREFIX]";
   exit 1;
 fi
 
 BIN=$1
 DATASET_DIR=$2
+OUTFILE_PREFIX=$3
 
 for VID_DIR in $DATASET_DIR/[0-9]
 do for SNIP_DIR in $VID_DIR/[0-9][0-9]
   do
     LOSSY_VID_FILE=$SNIP_DIR/pl.mp4
     LABEL_FILE=$SNIP_DIR/block.bytes
-    OUTPUT_FILE=$SNIP_DIR/features.bytes
+    OUTPUT_FILE=$SNIP_DIR/$OUTFILE_PREFIX.bytes
     echo $BIN $LOSSY_VID_FILE $LABEL_FILE $OUTPUT_FILE
     $BIN $LOSSY_VID_FILE $LABEL_FILE $OUTPUT_FILE
   done
