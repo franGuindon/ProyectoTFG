e747201497632cc906f2eb3a32d2a2da66673e1d
franGuindon <fbadilla10@gmail.com>
2023-05-13 16:47:57 -0600

    Refactor and add predict to analizer

--- tools/cpp_tools/analyzer/analyzer.cpp
+++ tools/cpp_tools/analyzer/analyzer.cpp
@@ -1,9 +1,13 @@
 /* Copyright 2023 Francis Guindon <fbadilla10@gmail.com> */
 
-#include <ForestClassification.h>
+#define DEBUG_LEVEL 3
 #include <stdio.h>
 
 #include <memory>
+
+#include "ForestClassification.h"
+#include "artifact_detector/include/logging.hpp"
+#include "artifact_detector/include/utility.hpp"
 
 using ranger::DEFAULT_ALPHA;
 using ranger::DEFAULT_IMPORTANCE_MODE;
@@ -58,24 +62,51 @@ struct Args {
   bool write = false;
 };
 
-int main(int argc, char** argv) {
-  if (argc != 3) {
-    if (argc == 5 && argv[2][0] == '2' && argv[2][1] == 0x00) {
-      goto skip_help;
-    }
+bool check_args(int argc, char** argv) {
+  if (argc < 3) {
+    return false;
+  }
 
+  int option = std::stoi(argv[2]);
+
+  if (option == 2) {
+    if (argc != 5) {
+      return false;
+    }
+  } else if (option == 4) {
+    if (argc < 5) {
+      return false;
+    }
+    if (argc % 2 != 1) {
+      return false;
+    }
+  } else {
+    if (argc != 3) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main(int argc, char** argv) {
+  if (!check_args(argc, argv)) {
     printf("Usage: analyzer [MODEL_FILE] [PRINT_TYPE] \n");
     printf("  PRINT_TYPE:\n");
     printf("           0: Feature count\n");
     printf("           1: Complete\n");
-    printf(
-        "           2: Single tree up to maxdepth (Requires additional args: "
-        "[TREE_ID] [MAXDEPTH])\n");
+    printf("           2: Single tree up to maxdepth\n");
+    printf("              (Requires additional args: [TREE_ID] [MAXDEPTH])\n");
     printf("           3: Depth (for each leaf node)\n");
+    printf("           4: Predict\n");
+    printf(
+        "              (Requires additional args: [FEATURE_FILE] [LABEL_FILE] "
+        "...)\n");
     return 1;
   }
 
-skip_help:
+  int option = std::stoi(argv[2]);
+
   auto forest = std::make_unique<ForestRangerx>();
 
   Args arg_handler{};
@@ -88,12 +119,93 @@ skip_help:
   arg_handler.predict = argv[1];
   arg_handler.maxdepth = 10;
 
-  const size_t features_per_block = 132;
-  const size_t num_rows = 1;
-  const size_t num_cols = features_per_block;
-  const size_t mem_size = num_rows * num_cols;
-  auto x_mem = std::unique_ptr<float[]>(new float[mem_size]);
-  auto y_mem = std::unique_ptr<float[]>(new float[num_rows]);
+  //----------------------------------------------------------------------------
+
+  size_t num_rows = 0;
+  size_t num_cols = 0;
+  std::unique_ptr<float[]> x_mem = nullptr;
+  std::unique_ptr<float[]> y_mem = nullptr;
+
+  size_t total_label_mem_size = 0;
+
+  if (option == 4) {
+    uint64_t now = GetCurrentTimeSinceEpochUs();
+
+    const size_t block_dimension = 16;
+    const size_t frame_width = 1280;
+    const size_t frame_height = 720;
+    const size_t blocks_per_row = frame_width / block_dimension;
+    const size_t blocks_per_col = frame_height / block_dimension;
+    const size_t blocks_per_frame = blocks_per_row * blocks_per_col;
+    const size_t num_frames_per_snip = 200;
+    const size_t features_per_block = 132;
+
+    const size_t label_file_size =
+        blocks_per_row * blocks_per_col * num_frames_per_snip;
+    const size_t num_file_rows =
+        (blocks_per_row - 2) * (blocks_per_col - 2) * num_frames_per_snip;
+    const size_t num_file_cols = features_per_block;
+    const size_t feature_file_size = num_file_rows * num_file_cols;
+    const size_t num_file_pairs = (argc - 3) / 2;
+    const size_t total_feature_mem_size = feature_file_size * num_file_pairs;
+    const size_t total_raw_label_mem_size = label_file_size * num_file_pairs;
+    total_label_mem_size = num_file_rows * num_file_pairs;
+
+    num_rows = total_label_mem_size;
+    num_cols = num_file_cols;
+
+    auto x_mem_raw =
+        std::unique_ptr<float[]>(new float[total_feature_mem_size]);
+    auto y_mem_raw = std::unique_ptr<unsigned char[]>(
+        new unsigned char[total_raw_label_mem_size]);
+
+    x_mem = std::unique_ptr<float[]>(new float[total_feature_mem_size]);
+    y_mem = std::unique_ptr<float[]>(new float[total_label_mem_size]);
+
+    size_t y_offset = 0;
+    for (size_t i = 0; i < num_file_pairs; ++i) {
+      printf("Loading feature file: %s\n", argv[3 + 2 * i]);
+      load_frame(argv[3 + 2 * i], x_mem_raw.get() + i * feature_file_size,
+                 feature_file_size);
+      printf("Loading label file: %s\n", argv[4 + 2 * i]);
+      load_frame(argv[4 + 2 * i], y_mem_raw.get() + i * label_file_size,
+                 label_file_size);
+
+      printf("Converting feature file from row major to column major\n");
+      for (size_t j = 0; j < num_file_rows; ++j) {
+        for (size_t k = 0; k < num_file_cols; ++k) {
+          float feature =
+              x_mem_raw[i * feature_file_size + j * num_file_cols + k];
+          x_mem[k * total_label_mem_size + (i * num_file_rows + j)] = feature;
+        }
+      }
+
+      printf("Building label memory\n");
+      for (size_t j = 0; j < num_frames_per_snip; ++j) {
+        for (size_t k = 1; k < blocks_per_col - 1; ++k) {
+          for (size_t l = 1; l < blocks_per_row - 1; ++l) {
+            size_t offset = i * label_file_size + j * blocks_per_frame +
+                            k * blocks_per_row + l;
+            y_mem[y_offset] = static_cast<float>(y_mem_raw[offset]);
+            ++y_offset;
+          }
+        }
+      }
+    }
+    if (y_offset != total_label_mem_size) {
+      printf("Mismatch between processed label size and total label size\n");
+      return -1;
+    }
+
+    printf("Dataset loading duration: %ld\n",
+           GetCurrentTimeSinceEpochUs() - now);
+  } else {
+    num_rows = 1;
+    num_cols = 132;
+    x_mem = std::unique_ptr<float[]>(new float[num_rows * num_cols]);
+    y_mem = std::unique_ptr<float[]>(new float[num_rows]);
+  }
+  //----------------------------------------------------------------------------
 
   printf("Initializing Rangerx\n");
   forest->initCppFromMem(
@@ -109,11 +221,46 @@ skip_help:
       arg_handler.holdout, arg_handler.predictiontype, arg_handler.randomsplits,
       arg_handler.maxdepth, arg_handler.regcoef, arg_handler.usedepth);
 
-  if (argc == 3) {
+  if (0 == option || 1 == option || 3 == option) {
     forest->print(static_cast<Tree::PrintType>(std::stoi(argv[2])));
-  } else if (argc == 5) {
+  } else if (2 == option) {
     forest->print(static_cast<Tree::PrintType>(std::stoi(argv[2])),
                   std::stoi(argv[3]), std::stoi(argv[4]));
+  } else if (4 == option) {
+    forest->run(true, true);
+    auto pred_mem = std::unique_ptr<float[]>(new float[total_label_mem_size]);
+    forest->get_predictions(pred_mem.get(), total_label_mem_size);
+
+    size_t true_positives = 0;
+    size_t true_negatives = 0;
+    size_t false_positives = 0;
+    size_t false_negatives = 0;
+
+    for (size_t i = 0; i < total_label_mem_size; ++i) {
+      if (255.0F == y_mem[i] && 255.0F == pred_mem[i]) {
+        ++true_positives;
+      } else if (0.0F == y_mem[i] && 0.0F == pred_mem[i]) {
+        ++true_negatives;
+      } else if (0.0F == y_mem[i] && 255.0F == pred_mem[i]) {
+        ++false_positives;
+      } else if (255.0F == y_mem[i] && 0.0F == pred_mem[i]) {
+        ++false_negatives;
+      } else {
+        printf("Found invalid values, id: %ld, label: %f, pred: %f\n", i,
+               y_mem[i], pred_mem[i]);
+      }
+    }
+    printf("Confusion matrix:\n");
+    printf("              0   255\n");
+    printf("predicted 0   %ld %ld\n", true_negatives, false_negatives);
+    printf("predicted 255 %ld %ld\n", false_positives, true_positives);
+    printf("\nPrecision: %f\n",
+           static_cast<float>(true_positives) /
+               static_cast<float>(true_positives + false_positives));
+    printf("\nRecall:    %f\n",
+           static_cast<float>(true_positives) /
+               static_cast<float>(true_positives + false_negatives));
   }
+
   return 0;
 }
